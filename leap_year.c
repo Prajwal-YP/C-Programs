e46ab0ecf975c5af2888896448f3c75d2b95cced
Prajwal_Y_P <87578946+Prajwal-YP@users.noreply.github.com>
2021-12-03 18:08:15 +0530

    Update leap_year.c

--- leap_year.c
+++ leap_year.c
@@ -1,4 +1,8 @@
-//Write a Program to Check whether the inputted year is leap year not.
+/*
+	Pragram Name... : Leap_year.c
+	Author......... : Prajwal Y P
+	Program Task... : To Check whether the inputted year is a leap year.
+*/
 
 #include<stdio.h>
 main()
@@ -8,10 +12,10 @@ main()
 	printf("Enter your present year.... : ");
 	scanf("%d",&y);
 	
-	if( (y%4==0 && y%100!=0) || y%400==0)
-		printf("\n\tYear \'%d\' is a Leap year.",y);
+	if( (y%4==0 && y%100!=0) || (y%400==0) )
+		printf("\n\t\'%d\' is a Leap year.",y);
 	else
-		printf("\n\tYear \'%d\' is not a Leap year.",y);	
+		printf("\n\t\'%d\' is not a Leap year.",y);	
 	
-	printf("\n\nNOTE : If it is a century year, than it is a leap year only ;\n\tif that year is DIVISIBLE BY \"400\" not \"4\" ");	
+	printf("\n\nNOTE : If it is a century year, than it is a leap year only if that year is DIVISIBLE BY \"400\" not \"4\" .");	
 }
